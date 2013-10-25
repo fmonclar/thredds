a8b190de6010748f3376723fe3d3db5759bfec5c
Dennis Heimbigner <dmh@ucar.edu>
2013-10-25 16:53:10 -0600

    It turns out that several classes, notably Variable, override Object.hashCode() to use the content of the object to construct the hash code -- as opposed to using its internal address, which is what Object.hashCode() does. This has an unfortunate consequence that if the object is being constructed piecemeal (as it may be, for example, in parsing), that its hash code will change over time.  If during that period, the object is being used as a key to a Map, then attempts to probe the map may return null because the Map code is looking in the wrong place. Their is a similar issue with Object.equals().
    I attempted to modify the code to use the standard
    Object.hashCode() and Object.equals(), but it turns
    out that this is embedded so deeply into the thredds
    code that it is almost impossible to remove. So, I am
    backing it out and will use other solutions.

--- cdm/src/main/java/ucar/nc2/EnumTypedef.java
+++ cdm/src/main/java/ucar/nc2/EnumTypedef.java
@@ -190,14 +190,6 @@ public class EnumTypedef extends CDMNode {
 
   @Override
   public boolean equals(Object o) {
-    if(CDMNode.OBJECTHASH)
-	return super.equals(o);
-    else
-	return equivalent(o);
-  }
-
-  @Override
-  public boolean equivalent(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
 
